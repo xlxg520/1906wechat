072a07277a29df71cede3e719788908133a06050
xlxg520 <1423952157@qq.com>
2020-01-14 19:39:21 +0800

    access_token

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -9,12 +9,24 @@ class TextController extends Controller
 {
    public function getAccessToken()
    {
+       $redis_weixin_token_key = 'weixin_access_token';
+        //判断是否 有缓存
+       $token = Redis::get($redis_weixin_token_key);
+       if($token){
 
-       $url ="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx1c2d3bdcfe3a747c&secret=65bca24ff0d89b7901e915967af766e6";
-       $json = file_get_contents($url);
-       $arr = json_decode($json,true);
-       echo $json;echo '</br>';
-       echo '<pre>';print_r($arr);echo '</pre>';
+           return $token;
+       }else{
+           //获取 微信access_token
+           $url ="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx1c2d3bdcfe3a747c&secret=65bca24ff0d89b7901e915967af766e6";
+           $json = file_get_contents($url);
+           $arr = json_decode($json,true);
+           $toekn = $arr['access_token'];
+       }
+
+       //缓存token
+       Redis::set($redis_weixin_token_key,$arr['access_token']);
+       Redis::expire($redis_weixin_token_key,7200);
+       return $token;
    }
 
 
